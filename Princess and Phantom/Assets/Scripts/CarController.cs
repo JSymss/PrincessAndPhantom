f3d34ad7d07f55ad91304d2fd98bb095bc2f23bf
Jackson <200341@student.sae.edu.au>
2021-04-16 20:28:28 +1000

    Updated car physics

--- Princess and Phantom/Assets/Scripts/CarController.cs
+++ Princess and Phantom/Assets/Scripts/CarController.cs
@@ -12,11 +12,16 @@ public class CarController : MonoBehaviour
 
     public float airDrag;
     public float groundDrag;
+    public float smoothCarRotation = 1f;
+    public float gravity;
 
     public float fwdSpeed;
     public float revSpeed;
     public float turnSpeed;
     public LayerMask groundLayer;
+
+    public Transform leftFrontWheel, rightFrontWheel;
+    public float maxWheelTurn = 25;
 
     void Start()
     {
@@ -45,8 +50,14 @@ public class CarController : MonoBehaviour
         isCarGrounded = Physics.Raycast(transform.position, -transform.up, out hit, 1f, groundLayer);
 
         // rotate car to be parallel to ground
-        transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        
+        //transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation, smoothCarRotation*Time.deltaTime);
 
+        // turn wheels
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.transform.Rotate(1f, 0, 0);
         if (isCarGrounded)
         {
             sphereRB.drag = groundDrag;
@@ -55,18 +66,24 @@ public class CarController : MonoBehaviour
         {
             sphereRB.drag = airDrag;
         }
+
     }
     private void FixedUpdate()
     {
+        // add extra gravity
+        sphereRB.AddForce(0, -gravity, 0);
+
         if (isCarGrounded)
         {
             // move car
+            turnSpeed = 100f;
             sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
         }
         else
         {
-            // add extra gravity
-            sphereRB.AddForce(transform.up * -30f);
+            turnSpeed = 30f;
+            // add engine weight
+            transform.Rotate(.8f, 0, 0);
         }
         
         

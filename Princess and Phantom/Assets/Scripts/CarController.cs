fce3dcd4caed672d6a9fadbf09a59a2f18c50fd0
Jackson <200341@student.sae.edu.au>
2021-04-21 18:45:34 +1000

    Car surfaces enums added

--- Princess and Phantom/Assets/Scripts/CarController.cs
+++ Princess and Phantom/Assets/Scripts/CarController.cs
@@ -8,10 +8,13 @@ public class CarController : MonoBehaviour
 
     float moveInput;
     float turnInput;
-    bool isCarGrounded;
 
-    public float airDrag;
     public float groundDrag;
+    public float iceDrag;
+    public float waterDrag;
+    public float tracksDrag;
+    public float airDrag;
+
     public float smoothCarRotation = 1f;
     public float gravity;
 
@@ -19,14 +22,21 @@ public class CarController : MonoBehaviour
     public float revSpeed;
     public float turnSpeed;
     public LayerMask groundLayer;
+    public LayerMask iceLayer;
+    public LayerMask waterLayer;
+    public LayerMask tracksLayer;
 
     public Transform leftFrontWheel, rightFrontWheel;
     public float maxWheelTurn = 25;
+
+    public enum Surface { Ground, Ice, Water, Tracks, Air };
+    Surface mySurface;
 
     void Start()
     {
         // detatch the rigidbody from the car
         sphereRB.transform.parent = null;
+        mySurface = Surface.Ground;
     }
 
 
@@ -45,47 +55,101 @@ public class CarController : MonoBehaviour
         float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
         transform.Rotate(0, newRotation, 0,Space.World);
 
+        
+        // change surface state
+        if(Physics.Raycast(transform.position, -transform.up, 1f, groundLayer))
+        {
+            mySurface = Surface.Ground;
+        }
+        if (Physics.Raycast(transform.position, -transform.up, 1f, iceLayer))
+        {
+            mySurface = Surface.Ice;
+        }
+        if (Physics.Raycast(transform.position, -transform.up, 1f, waterLayer))
+        {
+            mySurface = Surface.Water;
+        }
+        if (Physics.Raycast(transform.position, -transform.up, 1f, tracksLayer))
+        {
+            mySurface = Surface.Tracks;
+        }
+        if (Physics.Raycast(transform.position, -transform.up, 1f) == false)
+        {
+            mySurface = Surface.Air;
+        }
+
         // raycast ground check
         RaycastHit hit;
-        isCarGrounded = Physics.Raycast(transform.position, -transform.up, out hit, 1f, groundLayer);
-
+        Physics.Raycast(transform.position, -transform.up, out hit, 1f);
         // rotate car to be parallel to ground
-        
-        //transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        // stiff rotation in comment below
+        // transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation, smoothCarRotation*Time.deltaTime);
 
         // turn wheels
         leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, leftFrontWheel.localRotation.eulerAngles.z);
         rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightFrontWheel.localRotation.eulerAngles.z);
+        
+        // spin the wheels
         rightFrontWheel.transform.Rotate(1f, 0, 0);
-        if (isCarGrounded)
+
+        // set variables based on surface type
+        if (mySurface == Surface.Ground)
         {
             sphereRB.drag = groundDrag;
+            turnSpeed = 100f;
+            fwdSpeed = 250f;
+            revSpeed = 150f;
         }
-        else
+        if (mySurface == Surface.Ice)
+        {
+            sphereRB.drag = iceDrag;
+            turnSpeed = 75f;
+            fwdSpeed = 250f;
+            revSpeed = 150f;
+        }
+        if (mySurface == Surface.Water)
+        {
+            sphereRB.drag = waterDrag;
+            turnSpeed = 75f;
+            fwdSpeed = 250f;
+            revSpeed = 150f;
+        }
+        if (mySurface == Surface.Tracks)
+        {
+            sphereRB.drag = tracksDrag;
+            turnSpeed = 50f;
+            fwdSpeed = 150f;
+            revSpeed = 75f;
+        }
+        if (mySurface == Surface.Air)
         {
             sphereRB.drag = airDrag;
+            turnSpeed = 30f;
         }
-
+        print(mySurface);
     }
     private void FixedUpdate()
     {
         // add extra gravity
         sphereRB.AddForce(0, -gravity, 0);
 
-        if (isCarGrounded)
+        // car handling for each surface
+        if (mySurface != Surface.Air)
         {
             // move car
-            turnSpeed = 100f;
             sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+
         }
-        else
+        // if raycast hits nothing (air)
+        if(mySurface == Surface.Air)
         {
-            turnSpeed = 30f;
             // add engine weight
-            transform.Rotate(.8f, 0, 0);
+            if (transform.rotation.x < 30 && transform.rotation.x > -85)
+            {
+                transform.Rotate(1f, 0, 0);
+            }
         }
-        
-        
+
     }
 }

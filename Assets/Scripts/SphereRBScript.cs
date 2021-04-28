cc679778f871be740d97c45aa15ddf816a8439cc
Nora Luck-Jennings <1018949@student.sae.edu.au>
2021-04-28 13:23:51 +1000

    Jetty Track moves under water
    Got the jetty track to move under the water once the player reaches the town and unlocks the speedboat

--- Assets/Scripts/SphereRBScript.cs
+++ Assets/Scripts/SphereRBScript.cs
@@ -6,6 +6,19 @@ using UnityEngine.SceneManagement;
 public class SphereRBScript : MonoBehaviour
 {
     public Animator anim;
+    GameObject jettyTrack, jettyTarget;
+    GameObject[] jettyJumps;
+    public float hiddenDistance;
+
+    private void Start()
+    {
+        jettyTrack = GameObject.FindGameObjectWithTag("JettyTrack");
+        jettyTarget = GameObject.FindGameObjectWithTag("Target");
+        jettyJumps = GameObject.FindGameObjectsWithTag("JettyJump");
+        jettyTarget.transform.position = new Vector3(jettyTrack.transform.position.x, -300f, jettyTrack.transform.position.z);
+        CarController.speedBoatUnlock = false;
+        CarController.respawnCheckpoint = 1;
+    }
 
     private void OnTriggerEnter(Collider col)
     {
@@ -15,6 +28,18 @@ public class SphereRBScript : MonoBehaviour
             Destroy(col.gameObject);
             CarController.sledgeUnlock = true;
             CarController.respawnCheckpoint = 2;
+            StartCoroutine(Wait());
+        }
+        else if (col.gameObject.name == "TownTrigger")
+        {
+            Destroy(col.gameObject);
+            foreach (GameObject ramp in jettyJumps)
+            {
+                Destroy(ramp);
+            }
+            CarController.speedBoatUnlock = true;
+            CarController.respawnCheckpoint = 2;
+            jettyTrack.transform.position = Vector3.MoveTowards(jettyTrack.transform.position, jettyTarget.transform.position, hiddenDistance);
             StartCoroutine(Wait());
         }
         if(col.gameObject.name == "DoorToIceLevel")
